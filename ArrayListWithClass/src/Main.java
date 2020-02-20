76b5a36c7a68f438719522547004b9aa553e8d8c
0x21106 <ayhan2015007@gmail.com>
2020-02-20 22:37:47 +0400

    Array List With Classes

--- ArrayListWithClass/src/Main.java
+++ ArrayListWithClass/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Customer> customers = new ArrayList<Customer>();
+
+        Customer taksim_dayi = new Customer(12, "Taksim", "Dayi");
+
+        customers.add(new Customer(1, "Ayxan", "Abdullayev"));
+        customers.add(new Customer(2, "Faiq", "Nebiagayev"));
+        customers.add(new Customer(3, "Agadadas", "Qardas"));
+        customers.add(taksim_dayi);
+
+        for(Customer customer : customers) {
+            System.out.println(customer.firstName);
+        }
+
+        System.out.println("-------------");
+
+        customers.remove(taksim_dayi);
+        customers.remove(new Customer(1, "Ayxan", "Abdullayev"));
+
+        for(Customer customer : customers) {
+            System.out.println(customer.firstName);
+        }
+
     }
 }
