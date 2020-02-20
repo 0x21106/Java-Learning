f17ae6034dbee2c0eed5e28d84e5f52dc31175ce
0x21106 <ayhan2015007@gmail.com>
2020-02-20 20:11:31 +0400

    Packages Demo

---
+++ PackageDemo/src/Main.java
@@ -0,0 +1,9 @@
+import matematik.DortIslem;
+
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int number = dortIslem.topla(5, 10);
+        System.out.println(number); 
+    }
+}
