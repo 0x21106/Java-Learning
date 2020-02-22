de7d999b22e1d9483204f0d1cc5315e6b8f27269
0x21106 <ayhan2015007@gmail.com>
2020-02-22 19:18:54 +0400

    HashMap

--- HashMapDemo/src/Main.java
+++ HashMapDemo/src/Main.java
@@ -1,6 +1,9 @@
+import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    HashMap<String, String> dict = new HashMap<String, String>();
+	    dict.put("book", "kitap");
+	    System.out.println(dict);
     }
 }
